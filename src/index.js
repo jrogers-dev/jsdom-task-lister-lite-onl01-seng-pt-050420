d850c8f3cc981a549a35860d58aa110eb22c4551
James Rogers <jrogers.dev@gmail.com>
2020-11-14 07:19:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let task = document.createElement("LI");
       task.innerText = input.value;
       taskList.appendChild(task);
+      input.value = "";
     }
   });
 });
