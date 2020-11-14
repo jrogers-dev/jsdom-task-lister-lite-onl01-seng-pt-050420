2f82f2dede93bc9643f680841f57aa4d512231e6
James Rogers <jrogers.dev@gmail.com>
2020-11-14 07:16:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.getElementById("create-task-form");
   let input = document.getElementById("new-task-description");
-  let tasklist = document.getElementById("tasks")
+  let tasklist = document.getElementById("tasks");
   
   
   form.addEventListener("submit", function(e) {
