278d45afbb1e10f1e95da63393b65d5aa1d66c1b
James Rogers <jrogers.dev@gmail.com>
2020-11-14 06:57:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form");
+  let input = document.getElementById("new-task-description");
+  let tasklist = document.getElementById("tasks")
+  
+  
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if(input.textContent !== "") {
+      
+    }
+  });
 });
